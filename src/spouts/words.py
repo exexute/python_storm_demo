c86db83db9fcab164f0b25b74cc6535213a1af5b
exexute <1528360120@qq.com>
2019-02-25 16:01:00 +0800

    fix: 通过python编写storm读取kafka的内容

--- src/spouts/words.py
+++ src/spouts/words.py
@@ -2,6 +2,7 @@ from itertools import cycle
 
 from streamparse import Spout
 from pykafka import KafkaClient
+from pykafka.common import OffsetType
 
 
 class WordSpout(Spout):
@@ -10,13 +11,14 @@ class WordSpout(Spout):
     def initialize(self, stormconf, context):
         self.logger.info("enter words initialize function")
         client = KafkaClient(hosts="10.129.7.121:9092")
-        topic = client.topics['firewalllog']
-        self.consumer = topic.get_balanced_consumer(consumer_group='firewalllog', auto_commit_enable=True)
+        topic = client.topics['auditlog']
+        self.consumer = topic.get_simple_consumer(consumer_group='auditlog_py', auto_commit_enable=True,
+            auto_offset_reset=OffsetType.EARLIEST)
 
     def next_tuple(self):
         msg = next(self.consumer)
-        self.logger.info("enter words next_tuple function")
+        self.logger.info("enter words next_tuple function"+str(msg.value))
         #self.logger.info(
         #        "read data {}".format(msg)
         #    )
-        self.emit([msg])
+        self.emit([msg.value])
