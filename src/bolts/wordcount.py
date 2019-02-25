25f75b8f967ea62368d07c65ea93e990de28da97
exexute <1528360120@qq.com>
2019-02-25 17:10:19 +0800

    modify: 修改kafka相关内容

--- src/bolts/wordcount.py
+++ src/bolts/wordcount.py
@@ -5,22 +5,18 @@ from streamparse import Bolt
 
 
 class WordCountBolt(Bolt):
-    outputs = ["word", "count"]
+    outputs = ["hostname"]
 
-    # 
+    # bolts初始化函数， 用于初始化
     def initialize(self, conf, ctx):
-        self.logger.info("enter wordcount initalize function")
         self.counter = Counter()
         self.pid = os.getpid()
         self.total = 0
 
     def _increment(self, word, inc_by):
-        self.logger.info("enter wordcount _increment function")
         self.counter[word] += inc_by
         self.total += inc_by
 
     def process(self, tup):
-        self.logger.info("enter wordcount process function")
-        word = tup.values[0]
-        self._increment(word, 10 if word == "dog" else 1)
-        self.emit([word, self.counter[word]])
+        self.logger.info(help(tup))
+        self.emit(["xss"])
--- src/spouts/words.py
+++ src/spouts/words.py
@@ -7,20 +7,18 @@ from pykafka.common import OffsetType
 
 class WordSpout(Spout):
     outputs = ["word"]
-    def consumer(self, hosts=None, topic=None, auto_commit_enable=None, auto_offset_reset=None):
+    def consumer(self, hosts=None, topic=None, auto_commit_enable=True, auto_offset_reset=OffsetType.EARLIEST):
         _client = KafkaClient(hosts=hosts)
         _topic = _client.topics[topic]
         _consumer = _topic.get_simple_consumer(consumer_group=topic+'_py', auto_commit_enable=auto_commit_enable, auto_offset_reset=auto_offset_reset)
         for msg in _consumer:
             yield msg.value
     def initialize(self, stormconf, context):
-        self.logger.info("enter words initialize function")
-        self.messages = self.consumer(hosts="10.129.7.121:9092", topic='auditlog', auto_commit_enable=True, auto_offset_reset=OffsetType.EARLIEST)
+        self.messages = self.consumer(hosts="10.129.7.121:9092", topic='auditlog')
 
     def next_tuple(self):
         msg = next(self.messages)
-        self.logger.info("enter words next_tuple function"+str(msg))
-        #self.logger.info(
-        #        "read data {}".format(msg)
-        #    )
+        self.logger.info(
+                "read data {}".format(msg)
+            )
         self.emit([msg])
