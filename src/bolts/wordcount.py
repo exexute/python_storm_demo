99602696ddfe38eb27ebd9e8464c83b79807bb81
exexute <1528360120@qq.com>
2019-02-28 12:01:45 +0800

    modify

--- src/bolts/wordcount.py
+++ src/bolts/wordcount.py
@@ -45,7 +45,7 @@ class WordCountBolt(Bolt):
         self.pid = os.getpid()
 
     def process(self, tup):
-        self.logger.info("enter tup")
+        self.logger.info(tup.values[0])
         '''
         self.logger.error(tup.values[0])
         message=parse(tup.values[0])
